6a73a1b222e9e38d4b90966f78f4c1365922575b
fidelisaboke <fidelisaboke@gmail.com>
2023-05-01 21:09:26 +0300

    Added arrays.php

---
+++ arrays.php
@@ -0,0 +1,13 @@
+<?php
+//Two ways of initializing arrays:
+$names = ["John", "Johnson"];
+$fruits = array("mango", "banana");
+
+//Print the contents of the array with their indexes:
+print_r($names);
+print_r($fruits);
+
+//Print a single element of an array:
+echo $names[1]."\n";
+echo $fruits[0];
+?>
