16e23d8a9a987171231aed4e4114ec7ae2f25bf1
fidelisaboke <fidelisaboke@gmail.com>
2023-05-04 17:07:09 +0300

    Included the abs() function

--- math/math_functions.php
+++ math/math_functions.php
@@ -12,3 +12,11 @@ $num1 = 145.7;
 $num2 = 53.32;
 echo "Rounding off ".$num1.": ". round(145.67)."\n"; # Prints 146
 echo "Rounding off ".$num2.": ". round(53.32)."\n"; # Prints #53
+
+//rand() - returns a random number:
+echo "Random number 1 generated: ". rand() ."\n"; # Prints out a completely random integer (limited by 0 and maximum value accepted by php)
+echo "Random number 2 generated: ". rand(50, 500). "\n"; # Prints out a random integer between 50 and 500 (inclusive)
+
+//abs() - returns the absolute value of a number:
+$num3 = -8987;
+echo "Absolute value of ".$num3." is: ". abs($num3);
