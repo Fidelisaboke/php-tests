b85fdbb3a4f7950d34f941ca4cf74913eef6e20e
fidelisaboke <fidelisaboke@gmail.com>
2023-05-10 08:52:08 +0300

    Added oop.php and modified arrays.php

--- arrays/arrays.php
+++ arrays/arrays.php
@@ -1,13 +1,29 @@
 <?php
-//Two ways of initializing arrays:
+//Two ways of initializing indexed arrays:
 $names = ["John", "Johnson"];
 $fruits = array("mango", "banana");
+$cars = ["Volvo", "BMW", "Toyota"];
+
+// Initializing associative arrays:
+$patients = ['A001'=>"Doe", 'A002'=>"Doeson", 'A003'=>"Jane", 'A004'=>"Janeson"];
 
 //Print the contents of the array with their indexes:
 print_r($names);
 print_r($fruits);
+print_r($cars);
+print_r($patients);
 
 //Print a single element of an array:
 echo $names[1]."\n";
-echo $fruits[0];
+echo $fruits[0]."\n";
+echo $cars[2]."\n";
+
+//Associative arrays:
+echo $patients['A003']."\n";
+
+//Check the data type of the sarray:
+var_dump($names);
+var_dump($fruits);
+var_dump($cars);
+var_dump($patients);
 ?>
