40acd3be7219f485a0c4038ae3c09b750f607369
fidelisaboke <fidelisaboke@gmail.com>
2023-05-12 07:24:16 +0300

    Updated database.php

--- database/database.php
+++ database/database.php
@@ -14,15 +14,36 @@ $conn = new mysqli("localhost","root","");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully.\n";
 
 // Create database
-$sql = "CREATE DATABASE myDB";
-if ($conn->query($sql) === TRUE) {
+/*
+if ($conn->query("CREATE DATABASE db_drug_dispense") === TRUE) {
   echo "Database created successfully";
 } else {
   echo "Error creating database: " . $conn->error;
 }
+*/
 
+// Create table tbl_patients in db_drug_dispense
+/*
+$sql = "CREATE TABLE db_drug_dispense.tbl_patients (
+  patient_ssn INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  patient_firstname VARCHAR(30) NOT NULL,
+  patient_surname VARCHAR(30) NOT NULL,
+  patient_address VARCHAR(60) NOT NULL,
+  patient_email VARCHAR(50) NOT NULL,
+  patient_phone VARCHAR(13) NOT NULL,
+  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  )";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "Table tbl_patients created successfully";
+  } else {
+    echo "Error creating table: " . $conn->error;
+  }
+  */
+
+// Close the database connection
 $conn->close();
 ?>
