7862780670d3f2d154a5398ae5b001bb0ae36ae6
fidelisaboke <fidelisaboke@gmail.com>
2023-05-12 06:58:54 +0300

    Added some comments

--- database/database.php
+++ database/database.php
@@ -1,6 +1,11 @@
 <?php
 
 //Database Operations with PHP (From w3schools):
+/*
+Ensure that php.ini file is enabled and correct path to php in environment variables
+before starting database operations.
+XAMPP Control Panel was used to start Apache Web Server and MySQL Server
+*/
 
 // Create connection
 $conn = new mysqli("localhost","root","");
