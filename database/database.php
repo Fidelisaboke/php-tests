d53679e384d338696116276b72def05a1e093e14
fidelisaboke <fidelisaboke@gmail.com>
2023-05-12 20:50:06 +0300

    Updated database.php

--- database/database.php
+++ database/database.php
@@ -14,7 +14,21 @@ $conn = new mysqli("localhost","root","");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully.\n";
+echo "Connected successfully.<br>";
+
+/*You can point to a specific database during corrections as follows:
+$servername = "localhost";
+$username = "";
+$password = "";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+*/
 
 // Create database
 /*
@@ -56,6 +70,76 @@ $sql = "CREATE TABLE db_drug_dispense.tbl_patients (
     echo "Error inserting record into the table: ".$conn->error;
   }
   */
+
+  // Running multiple queries (use $conn->multi_query($sql)):
+  /*
+  $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+  VALUES ('John', 'Doe', 'john@example.com');";
+  $sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+  VALUES ('Mary', 'Moe', 'mary@example.com');";
+  $sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+  VALUES ('Julie', 'Dooley', 'julie@example.com')";
+  
+  if ($conn->multi_query($sql) === TRUE) {
+    echo "New records created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  */
+
+  
+  /* Prepared statements - They are used to execute queries whereby the program fixed values in parameters
+  defined in the sql statement
+
+  // Prepare parameterized statement:
+  $statement = $conn->prepare("INSERT INTO db_drug_dispense.tbl_patients 
+  (patient_firstname, patient_surname, patient_address, patient_email, patient_phone)
+  VALUES (?, ?, ?, ?, ?)");
+
+  // Bind values to parameters
+  $statement->bind_param("sssss", $patient_firstname, $patient_surname, $patient_address, 
+  $patient_email, $patient_phone);
+
+    // Possible values for the first parameter in $statement->bind_param:
+    s - string (one 's' represents one string)
+    d - double
+    i - integer
+    B - BLOB
+    
+
+  // Set values to the variables:
+  $patient_firstname = "Jane";
+  $patient_surname = "Janeson";
+  $patient_address = "00100 - Nairobi, Kenya";
+  $patient_email = "janeson002@gmail.com";
+  $patient_phone = "+254712555555";
+
+  // Execute the statement:
+  if($statement->execute() === TRUE){
+    echo "Data entered into table successfully";
+  }else{
+    echo "Error inserting data: "."$conn->error";
+  }
+  */
+
+// Select rows form a table (check results on a web browser):
+  $sql = "SELECT * FROM db_drug_dispense.tbl_patients";
+  $result_set = $conn->query($sql);
+
+  if($result_set->num_rows>0){
+    $item_no = 1;
+    // Fetch the rows of the table (fetched in forms of associative arrays):
+      while($row = $result_set->fetch_assoc()){
+        echo "Row no: ".$item_no."<br>";
+        foreach($row as $key=>$value){
+          echo $key."-> ".$value."<br>";
+        }
+        $item_no++;
+      }
+  } else{
+    echo "No data present";
+  }
+  
 
 // Close the database connection
 $conn->close();
