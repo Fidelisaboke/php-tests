ff72f149e60bb267d98ec0a52d220818bf72638e
fidelisaboke <fidelisaboke@gmail.com>
2023-05-13 22:34:02 +0300

    Updated student.php

--- oop/student.php
+++ oop/student.php
@@ -1,21 +1,53 @@
 <?php
 class Student{
-    public $adm_no;
-    public $name;
+    //Attributes
+    private $adm_no;
+    private $student_name;
 
-    public function __construct($adm_no, $name){
-        $this->adm_no = $adm_no;
-        $this->name = $name;
+    //Constructor method that runs when an object is being created:
+    public function __construct(){
+        echo "An object of the student class has been created.";       
 
     }
 
+    //Destructor method that is called at the end of the script:
+    public function __destruct(){
+        echo "This is student no: ".$this->adm_no."\n";
+    }
+
+    // Setters and getters:
+    public function setAdmNo($adm_no){
+        $this->adm_no=$adm_no;
+    }
+    public function setStudentName($student_name){
+        $this->student_name = $student_name;
+    }
+    public function getAdmNo(){
+        return $this->adm_no;
+    }
+
+    public function getStudentName(){
+        return $this->student_name;
+    }
+
+    //Method(s):
+
     public function greetStudent(){
-        echo "Hello ".$this->name;
+        echo "Hello ".$this->student_name."\n";
     }
 }
 
-$student_one = new Student(1001, "Doe Doeson");
-echo "Student No: ".$student_one->adm_no."\n";
-echo "Student name: ".$student_one->name."\n";
+//Object instantiation:
+$student_one = new Student();
+var_dump($student_one instanceof Student); // Checking whether an object belongs to a class.
+
+//Set the attributes:
+$student_one->setAdmNo(1001);
+$student_one->setStudentName("Doe Doeson");
+
+//Get the attributes and display them:
+echo "Student No: ".$student_one->getAdmNo()."\n";
+echo "Student name: ".$student_one->getStudentName()."\n";
+
 echo $student_one->greetStudent();
 ?>
