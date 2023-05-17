5b05e8ce123ff9bfdf39037570f385f05ee36256
fidelisaboke <fidelisaboke@gmail.com>
2023-05-17 20:02:28 +0300

    Updated animal.php

--- oop/animal.php
+++ oop/animal.php
@@ -3,18 +3,26 @@
 abstract class Animal{
     // concrete method:
     public function eat(){
-        echo "All animals eat.";
+        echo "All animals eat.\n";
     }
     // abstract method:
-    public abstract function make_sound($animal_sound);
+    public abstract function make_sound();
 }
 class Cow extends Animal{
-    public function make_sound($animal_sound){
-        echo "Moo";
+    public function make_sound(){
+        echo "Moo\n";
     }
 } class Dog extends Animal{
-    public function make_sound($animal_sound){
-        echo "Woof!";
+    public function make_sound(){
+        echo "Woof!\n";
     }
 }
-    ?>
+
+// Creating objects
+$cow = new Cow();
+$dog = new Dog();
+$cow->eat();
+$cow->make_sound();
+$dog->eat();
+$dog->make_sound();
+?>
