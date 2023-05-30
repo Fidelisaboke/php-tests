19cd51b2916a460b8ff42d3a401da673482769d8
fidelisaboke <fidelisaboke@gmail.com>
2023-05-30 19:51:12 +0300

    Moved the php POST processing script to patients.php

--- patients_form/patients.php
+++ patients_form/patients.php
@@ -8,8 +8,8 @@
     <title>Patients Form</title>
     <style>
         body{
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-    background: aliceblue;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        background: aliceblue;
         }
         form {
             margin: 0% auto;
@@ -36,7 +36,60 @@
     </style>
 </head>
 <body>
-    <form id="patient-form" action="add_patients.php" method="POST">
+    <?php
+    /* This php script processes the POST request to get the values from the form and 
+    insert them as a row in the db */
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "db_drug_dispense";
+
+    // Create database connection
+    $conn = new mysqli($servername,
+        $username, $password, $dbname);
+
+    // Check and confirm the connection
+    if ($conn->connect_error) {
+        die("Connection failed: "
+            . $conn->connect_error);
+    } else{
+        echo "Connected to ".$dbname." successfully";
+    }
+
+
+    // Preparing a statement to insert the data:
+    $sql = "INSERT INTO tbl_patients (patient_ssn, patient_firstname, patient_surname, patient_address, patient_email, patient_phone)
+    VALUES (?, ?, ?, ?, ?, ?)"; // The sql parametized statement
+    $statement = $conn->prepare($sql); // prepare the sql statement
+
+    // Bind values to parameters:
+    $statement->bind_param("isssss", $patient_ssn, $patient_firstname, $patient_surname, $patient_address, $patient_email, $patient_phone);
+
+    // Obtain values from the input fields of the form using $_POST[]:
+    $patient_ssn = $_POST['patient_ssn'];
+    $patient_firstname = $_POST['patient_firstname'];
+    $patient_surname = $_POST['patient_surname'];
+    $patient_address = $_POST['patient_address'];
+    $patient_email = $_POST['patient_email'];
+    $patient_phone = $_POST['patient_phone'];
+
+    // Run the statement:
+    if($statement->execute()=== TRUE){
+        echo "<h2>Data entered into table successfully!<h2>";
+
+        // Insert HTML line breaks before all newlines in a string
+        echo nl2br("\n$patient_ssn\n $patient_firstname\n $patient_surname\n $patient_address\n$patient_email\n $patient_phone");
+    } else{
+        // Print an error message if data is not inserted
+        echo "Error in inserting the data: ".$conn->error;
+    }
+
+    ?>
+
+    <form id="patient-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+    <!-- In the action attribute, the value passed is the php script which outputs the name of the script
+    being run-->
         <header id="patient-form-header">
             <h3 id="patient-form-title">Patient Form</h3>
             <h4>Enter your details below</h4>
