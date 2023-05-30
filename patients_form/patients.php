f481b924cb5fd0ba732959f541fb237bf95e614c
fidelisaboke <fidelisaboke@gmail.com>
2023-05-30 20:49:21 +0300

    Made changes to patient.php

--- patients_form/patients.php
+++ patients_form/patients.php
@@ -42,7 +42,8 @@
         insert them as a row in the db */
         
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            
+            try{
+                
             $servername = "localhost";
             $username = "root";
             $password = "";
@@ -55,22 +56,22 @@
             // Check and confirm the connection
             if ($conn->connect_error) {
                 die("Connection failed: "
-                    . $conn->connect_error);
+                    . $conn->connect_error)."<br>";
             } else{
-                echo "Connected to ".$dbname." successfully";
+                echo "Connected to ".$dbname." successfully<br>";
             }
 
 
             // Preparing a statement to insert the data:
-            $sql = "INSERT INTO tbl_patients (patient_ssn, patient_firstname, patient_surname, patient_address, patient_email, patient_phone)
-            VALUES (?, ?, ?, ?, ?, ?)"; // The sql parametized statement
+            $sql = "INSERT INTO tbl_patients (patient_firstname, patient_surname, patient_address, patient_email, patient_phone)
+            VALUES (?, ?, ?, ?, ?)"; // The sql parametized statement
             $statement = $conn->prepare($sql); // prepare the sql statement
 
             // Bind values to parameters:
-            $statement->bind_param("isssss", $patient_ssn, $patient_firstname, $patient_surname, $patient_address, $patient_email, $patient_phone);
+            $statement->bind_param("sssss", $patient_firstname, $patient_surname, $patient_address, $patient_email, $patient_phone);
 
             // Obtain values from the input fields of the form using $_POST[]:
-            $patient_ssn = $_POST['patient_ssn'];
+            //$patient_ssn = $_POST['patient_ssn'];
             $patient_firstname = $_POST['patient_firstname'];
             $patient_surname = $_POST['patient_surname'];
             $patient_address = $_POST['patient_address'];
@@ -82,11 +83,16 @@
                 echo "<h2>Data entered into table successfully!<h2>";
 
                 // Insert HTML line breaks before all newlines in a string
-                echo nl2br("\n$patient_ssn\n $patient_firstname\n $patient_surname\n $patient_address\n$patient_email\n $patient_phone");
+                //echo nl2br("\n$patient_ssn\n $patient_firstname\n $patient_surname\n $patient_address\n$patient_email\n $patient_phone");
             } else{
                 // Print an error message if data is not inserted
                 echo "Error in inserting the data: ".$conn->error;
             }
+            }
+            catch (Exception){
+                echo "<h3>An error occurred while processing request. Check if your input is valid.</h3>";
+            }
+            
         }
 
 
@@ -99,20 +105,20 @@
             <h3 id="patient-form-title">Patient Form</h3>
             <h4>Enter your details below</h4>
         </header>
-        <label for="patient_ssn">SSN</label>
-        <input type="text" id="patient_ssn" name="patient_ssn" placeholder="SSN...">
+        <!--<label for="patient_ssn">SSN</label>
+        <input type="text" id="patient_ssn" name="patient_ssn" placeholder="SSN...">-->
         <label for="patient_firstname">First Name</label>
-        <input type="text" id="patient_firstname" name="patient_firstname" placeholder="First name...">
+        <input type="text" id="patient_firstname" name="patient_firstname" placeholder="First name..." required>
         <label for="patient_surname">Surname</label>
-        <input type="text" id="patient_surname" name="patient_surname" placeholder="Surnname...">
+        <input type="text" id="patient_surname" name="patient_surname" placeholder="Surname..." required>
         <label for="patient_dob">Date of Birth</label>
-        <input type="date" id="patient_dob" name="patient_dob">
+        <input type="date" id="patient_dob" name="patient_dob" required>
         <label for="patient_address">Address</label>
-        <input type="text" id="patient_address" name="patient_address" placeholder="Address...">
+        <input type="text" id="patient_address" name="patient_address" placeholder="Address..." required>
         <label for="patient_email">Email</label>
-        <input type="email" id="patient_email" name="patient_email" placeholder="someone@example.com">
+        <input type="email" id="patient_email" name="patient_email" placeholder="someone@example.com" required>
         <label for="patient_phone">Phone</label>
-        <input type="text" id="patient_phone" name="patient_phone" list="country-codes">
+        <input type="text" id="patient_phone" name="patient_phone" list="country-codes" required>
         <datalist id="country-codes">
             <option value="+254">Kenya</option>
             <option value="+255">Tanzania</option>
